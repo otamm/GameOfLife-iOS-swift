3f954e4f8e937ee4d84644e6c843ca2c055cb9b4
otamm <oivatom@gmail.com>
2015-03-18 17:41:29 -0300

    Added grid methods, creatures appearing in wrong places, check the engine's positioning

--- GameOfLife.spritebuilder/Grid.swift
+++ GameOfLife.spritebuilder/Grid.swift
@@ -8,21 +8,56 @@
 
 import UIKit
 
-class Grid: CCSprite {
-   
+  class Grid: CCSprite {
+        
     let GridRows = 8 // constant
     let GridColumns = 10 // constant
+        
+    var totalAlive = 0 // these two store the label values for the game stats in the current state.
+    var generation = 0
+        
+    private var cellWidth: CGFloat = 0 // private vars are not accessible to/from other classes.
+    private var cellHeight: CGFloat = 0 // define height & width of a grid cell
+    private var gridArray: [[Creatures]]! // actual storage of creatures in the grid
+        
+    // Add Grid methods below here
     
-    class Grid: CCSprite {
+    override func onEnter() { // this method is automatic in Swift. This code overrides the method.
+        super.onEnter() // handles touch events
         
-        var totalAlive = 0
-        var generation = 0
+        setupGrid()
         
-        private var cellWidth: CGFloat = 0 // private vars are not accessible to/from other classes.
-        private var cellHeight: CGFloat = 0
-        private var gridArray: [[Creature]]!
-        
-        // Add Grid methods below here
+        userInteractionEnabled = true
     }
     
-}
+    private func setupGrid() {
+        
+        cellWidth = contentSize.width / CGFloat(GridColumns) // calculates the size of the grid by dividing
+        cellHeight = contentSize.height / CGFloat(GridRows) //grid's size by the amount of rows & cols
+        
+        gridArray = []
+        
+        for row in 0..<GridRows {
+            
+            gridArray.append([]) // creates a new array for each row
+            
+            for column in 0..<GridColumns { // fills the array with a number of 'creature' objects equal to the number of columns in that row;
+                
+                var creature = Creatures() // creates a new instance of the Creature() class;
+                creature.position = CGPoint(x: cellWidth * CGFloat(column), // sets the 'position'
+                    y: cellHeight * CGFloat(row)) //attribute of current Creature class instance equal to
+                addChild(creature) // its position in the grid
+                
+                gridArray[row].append(creature) // pushes the creature instance to the current row array
+                
+                // make creatures visible to test this method,
+                // set to false once we know we have filled the grid properly
+                creature.isAlive = true;
+            }
+        }
+    }
+    
+  }
+
+    
+
