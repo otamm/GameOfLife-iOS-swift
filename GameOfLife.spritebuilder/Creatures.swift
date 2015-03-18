a34aac3e4e14967fd1f71f9bf9c7cee17f7c8c17
otamm <oivatom@gmail.com>
2015-03-18 02:27:57 -0300

    Added creatures CCSprite class

---
+++ GameOfLife.spritebuilder/Creatures.swift
@@ -0,0 +1,13 @@
+//
+//  Creatures.swift
+//  GameOfLife
+//
+//  Created by Otavio Monteagudo on 3/18/15.
+//  Copyright (c) 2015 Apportable. All rights reserved.
+//
+
+import UIKit
+
+class Creatures: CCSprite {
+   
+}
