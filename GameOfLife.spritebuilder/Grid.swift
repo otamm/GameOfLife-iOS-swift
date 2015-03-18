52eff9f0f21396f33c7e83cc63ff2917bf66809e
otamm <oivatom@gmail.com>
2015-03-18 02:27:03 -0300

    Added grid CCSprite class

---
+++ GameOfLife.spritebuilder/Grid.swift
@@ -0,0 +1,13 @@
+//
+//  Grid.swift
+//  GameOfLife
+//
+//  Created by Otavio Monteagudo on 3/18/15.
+//  Copyright (c) 2015 Apportable. All rights reserved.
+//
+
+import UIKit
+
+class Grid: CCSprite {
+   
+}
